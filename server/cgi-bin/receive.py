97745d2a38ed039e729b19ceef6fcdef99877b03
Arkadeep De <arkadeepde142@gmail.com>
2020-01-03 22:56:48 +0530

    Recieving Script

---
+++ server/cgi-bin/receive.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import cgi, cgitb, os
+import json
+
+cgitb.enable()
+
+try:
+    form = cgi.FieldStorage()
+    print("Cache-control: private, max-age=0, no-cache")
+    print("Content-type: application/json\r\n\r\n")
+    fileitem = form['filename']
+    if fileitem.filename:
+        fn = os.path.basename(fileitem.filename)
+        open("/recieved/" + fn, 'wb').write(fileitem.file.read())
+        message = 'The file name "' + fn + '" is transferred.'
+
+    data = {"message" : message}
+    jsonout = json.dumps(data)
+    print(jsonout)
+
+except:
+    cgitb.handler()

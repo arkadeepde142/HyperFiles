3ce3a2cac1cf88769884321f52a972d0e30fbfe1
Arkadeep De <arkadeepde142@gmail.com>
2020-01-05 22:52:17 +0530

    Send abspath error corrected

--- server/cgi-bin/send.py
+++ server/cgi-bin/send.py
@@ -8,7 +8,9 @@ cgitb.enable()
 try:
     print("Cache-control: private, max-age=0, no-cache")
     print("Content-type: application/json\r\n\r\n")
-    filelist = os.listdir("../send/")
+    basepath = os.path.dirname(__file__)
+    path = os.path.abspath(os.path.join(basepath, "..", "send"))
+    filelist = os.listdir(path)
     filelist = filter(lambda x: (x[0] != '.' and x!='cgi-bin'), filelist)
     filelist = list(filelist)
     filedict = {'files' : filelist}
